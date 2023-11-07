2ad53e93f1a053ef4572478bfd44b25e66340839
Elbadil <elbaliadil@gmail.com>
2023-11-07 18:36:02 +0100

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defining a function top_ten"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts
+    listed for a given subreddit"""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+
+    headers = {'User-Agent': 'MyAPI'}
+
+    req = requests.get(url, headers=headers, allow_redirects=False)
+
+    if req.status_code == 200:
+        hot_data = req.json()
+        hot_data_posts = hot_data['data']['children']
+        count = 10
+        for post in hot_data_posts:
+            print(post['data']['title'])
+            count -= 1
+            if count == 0:
+                break
+    else:
+        print('None')
